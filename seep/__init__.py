f85fc5752acca49ed43d66111e7dbbdab34571a3
Julian Berman <Julian@GrayVines.com>
2018-11-17 09:29:54 -0500

    Modernized setup.py

--- seep/__init__.py
+++ seep/__init__.py
@@ -1,1 +1,5 @@
-from seep._version import __version__
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
